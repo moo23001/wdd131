b7a111fd3c04caf86461a0f03093ecd352f04971
pedromoo <moo23001@byui.edu>
2024-06-06 03:03:53 -0600

    Corrected lastModified date and formatted footer

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,4 +5,4 @@ const today = new Date();
 year.innerText = today.getFullYear();
 let oLastModif = new Date(document.lastModified);
 
-document.getElementById("lastModified").innerHTML = `Last modfied on: ${oLastModif}`;
+document.getElementById("lastModified").innerHTML = `Last modfied on: ${Intl.DateTimeFormat("en-US",{dateStyle:"full"}).format(oLastModif)}`;

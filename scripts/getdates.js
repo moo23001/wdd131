6d09f8c13dc0b3c4f80ca23e3b303182b5a42738
pedromoo <moo23001@byui.edu>
2024-06-06 02:18:26 -0600

    Added copyright year and Last modified on date

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,6 @@ const year = document.querySelector("#currentyear");
 const today = new Date();
 
 year.innerText = today.getFullYear();
+let oLastModif = new Date(document.lastModified);
+
+document.getElementById("lastModified").innerHTML = `Last modfied on: ${oLastModif}`;

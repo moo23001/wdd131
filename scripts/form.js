d36b085c9220da55633fb3388fb3132a1b15151c
pedromoo <moo23001@byui.edu>
2024-07-11 01:24:40 -0600

    Form assignment

---
+++ scripts/form.js
@@ -0,0 +1,36 @@
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
+const productName = document.querySelector('#productName');
+
+products.forEach(function(product) {
+    const optionLine = document.createElement("option");
+    optionLine.value = product.id
+    optionLine.textContent = product.name
+    productName.appendChild(optionLine);
+})

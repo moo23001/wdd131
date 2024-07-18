a8528a91500285333dddfde047fcfa0fc35e0c5e
pedromoo <moo23001@byui.edu>
2024-07-17 19:12:10 -0600

    LocalStorage corrected

--- scripts/form.js
+++ scripts/form.js
@@ -34,3 +34,47 @@ products.forEach(function(product) {
     optionLine.textContent = product.name
     productName.appendChild(optionLine);
 })
+
+let formAnswers = [];
+const postReviewButton = document.getElementById('postReview');
+postReviewButton.addEventListener('click', 
+  function(event) {
+    //event.preventDefault();
+
+    const productNameValue = productName.value;
+    const reviewDate = document.getElementById('date').value;
+    const writtenReview = document.getElementById('writtenReview').textContent;
+    const userName = document.getElementById('userName').value;
+    const rating = document.querySelector('input[name="stars"]:checked').value;
+    const features = [];
+    document.querySelectorAll('input[name="features"]:checked').forEach(function(checkbox) {
+      features.push(checkbox.value)
+    })
+
+    const formData = {
+      productName: productNameValue,
+      ratting: rating,
+      reviewDate: reviewDate,
+      features: features,
+      writtenReview: writtenReview,
+      userName: userName
+
+    }
+    setFormAnswers(formData);
+    console.log(formData)
+  }
+)
+
+
+
+function setFormAnswers (formData) {
+  formAnswers.push(formData)
+  localStorage.setItem('formSubmision', JSON.stringify(formAnswers));
+  
+  console.log(formAnswers)
+}
+
+function getFormAnswers() {
+  JSON.parse(localStorage.getItem('formSubmision'));
+}
+
